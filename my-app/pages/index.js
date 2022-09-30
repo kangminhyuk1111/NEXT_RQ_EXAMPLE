c49eded92050c59fc56bc430da3edb94b0f7e89b
kangminhyuk1111 <rkdglqkr12@naver.com>
2022-09-30 17:07:33 +0900

    20220930-ubn-4

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -2,13 +2,8 @@ import axios from "axios";
 import { useQuery } from "react-query";
 import React from 'react';
 
-const API_KEY = '4r2MSxI1MaeVEVdIYloEsxJZ2vJCQfP7vIxD4hPgXUtbPbmIXylX3KSmBRqxqjfKqgQ24xag4QEY%2F%2FzT2hq82w%3D%3D';
-
 const fetchingData = async() =>{
-  const data = await axios.get(`http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst
-  ?serviceKey=${API_KEY}&numOfRows=10&pageNo=1
-  &base_date=20210628&base_time=0600&nx=55&ny=127
-  `)
+  const data = await axios.get(`/`)
   console.log(data);
 }
 
