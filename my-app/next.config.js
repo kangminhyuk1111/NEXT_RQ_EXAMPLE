2af6924d77eab0be1b5372620821a76796a7661e
kangminhyuk1111 <rkdglqkr12@naver.com>
2022-09-30 15:20:05 +0900

    20220930-ubn-3

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,7 +1,13 @@
+//next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `http://apis.data.go.kr/:path*`,
+      },
+    ];
+  },
+};
